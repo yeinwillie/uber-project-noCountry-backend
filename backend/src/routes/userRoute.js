ccc8c4d3b7eda7325e0a75c7f4fd577e3f3bc121
Yein E <yeinneg@gmail.com>
2023-08-31 19:45:25 -0300

    token recoverytoken

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -11,7 +11,7 @@ router.post('/id', validateTokenMiddleware, usersGetById);
 
 router.patch('/editarUsuario',validateTokenMiddleware,usersPut);
 
-router.post('/recuperarPassword',validateTokenMiddleware,recoverypassword);
+router.post('/recuperarPassword',recoverypassword);
 
 router.delete('/:id', validateTokenMiddleware, usersDelete);
 
